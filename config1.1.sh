b7eed3566ff32458e9d588f3eda075a7165f4056
Tomas Caetano <65103987+caf-3@users.noreply.github.com>
2021-08-31 16:15:40 +0200

    Update config1.1.sh

--- config1.1.sh
+++ config1.1.sh
@@ -14,10 +14,5 @@ systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
 
-# run jenkins
-mkdir -p /var/jenkins_home
-chown -R 1000:1000 /var/jenkins_home/
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:latest
-
 # show endpoint
-echo 'Jenkins installed'
+echo 'Dcoker installed'
