f4a0ab0c9b97fef081f2ed7a9e7da5242b3ac11c
Tomas Caetano <65103987+caf-3@users.noreply.github.com>
2021-09-01 16:41:41 +0200

    Update config2.1.sh

--- config2.1.sh
+++ config2.1.sh
@@ -1,24 +1,31 @@
-#!/bin/bash
+FROM jenkins/jenkins
 
-# this script is only tested on ubuntu xenial
+ENV JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
 
-# install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
-systemctl enable docker
-systemctl start docker
-usermod -aG docker ubuntu
+ARG GIT_COMMIT=unspecified
+LABEL git_commit=$GIT_COMMIT
+# Run this command to find git commit:-
+#docker inspect quay.io/shazchaudhry/docker-jenkins | jq '.[].ContainerConfig.Labels'
 
-# run jenkins
-mkdir -p /var/jenkins_home
-chown -R 1000:1000 /var/jenkins_home/
-docker run -p 8080:8080 -p 50000:50000 -d -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+# Configure Jenkins
+COPY config/*.xml $JENKINS_HOME/
+COPY config/*.groovy /usr/share/jenkins/ref/init.groovy.d/
 
-# show endpoint
-echo 'Jenkins installed'
-echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
+USER root
+
+# Install Docker from official repo
+RUN apt-get update -qq && \
+    apt-get install -qqy apt-transport-https ca-certificates curl gnupg2 software-properties-common && \
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
+    apt-key fingerprint 0EBFCD88 && \
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && \
+    apt-get update -qq && \
+    apt-get install -qqy docker-ce && \
+    usermod -aG docker jenkins && \
+    chown -R jenkins:jenkins $JENKINS_HOME/
+
+USER jenkins
+
+VOLUME [$JENKINS_HOME, "/var/run/docker.sock"]
+
+echo 'Done'
